8fb4a8f09f7959dd686f5897ba1555f9142ad2b4
Lucio Carnevalle <luciocarnevalle12@gmail.com>
2025-11-11 14:22:28 -0300

    Probando Rutas

--- js/index.js
+++ js/index.js
@@ -3,13 +3,13 @@ import { navbar } from "./navbar.js";
 document.getElementById("btnCerrar").addEventListener("click", function (e) {
   e.preventDefault(); // evito que recargue o siga un link
 
-  // 1. Muestra el cartel de confirmación
+  //Muestro el cartel de confirmación
   const estaSeguro = window.confirm("¿Estás seguro que quieres cerrar sesión?");
 
-  // 2. Si el usuario hizo clic en "Aceptar" (estaSeguro es true)...
+  //Si el usuario hizo clic en "Aceptar" (estaSeguro es true)
   if (estaSeguro) {
     localStorage.removeItem("rememberedEmail"); //borro los datos de sesion
-    window.location.href = "./pages/login.html"; //...lo redirigimos
+    window.location.href = "./pages/login.html"; //lo mando a login
   }
   // Si hace clic en "Cancelar", no se hace nada.
 });
